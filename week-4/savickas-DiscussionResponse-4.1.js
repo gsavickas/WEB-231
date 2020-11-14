8bd74c045c2b28aed4b53641a24edef726ab6415
Grayton Savickas <gsavickaspro@gmail.com>
2020-11-13 19:34:47 -0600

    added discussion response

---
+++ week-4/savickas-DiscussionResponse-4.1.js
@@ -0,0 +1,20 @@
+/**
+ * Title: Discussion 4.1
+ * Author: Ethan Kenneth Winter
+ * Date: 12 November 2020
+ * Description: Create a JS array with at least two errors
+ */
+
+// let header = require('../week-2/winter-header.js');
+
+// console.log(header.display("Ethan Kenneth", "Winter", "Discussion 4.1 Arrays"));
+
+// variable array listing holidays
+
+// Corrected: fixed element separators from ; to the syntactically proper , 
+// Corrected all elements to be strings
+let holidays = ["New Years", "Valentines", "April Fools", "4th of July", "Halloween", "Thanksgiving", "Christmas"];
+
+
+//log holidays array
+console.log(holidays);
