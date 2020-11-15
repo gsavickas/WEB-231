f5dbd41c7fcccd6e6d2703bd4d0457f7a95d826e
Grayton Savickas <graytonsavickas@Mandalorian.local>
2020-11-15 14:30:18 -0600

    Completed Exercise 4.2

--- week-2/Savickas-header-2.2.js
+++ week-2/Savickas-header-2.2.js
@@ -5,6 +5,7 @@
 ; Date:   31 Oct 2020
 ; Modified By: Grayton Savickas
 ; Description: Displays a formatted header
+; Attribution: JavaScript: The Definitive Guide, 7th Edition by David Flanagan Published by O'Reilly Media, Inc., 2020 
 ;===========================================
 */
 
@@ -17,4 +18,4 @@
 const header = require('../week-2/Savickas-header')
 // Tests the import of header function works
 var headerValue = header.display("Grayton", "Savickas", "Header")
-console.log(headerValue);
+console.log(headerValue + "\n");
--- week-4/savickas-Assignemnt-4.2-Arrays.js
+++ week-4/savickas-Assignemnt-4.2-Arrays.js
@@ -1,14 +1,32 @@
 /*
 ============================================
-; Title:  header.js
+; Title:  Arrays
 ; Author: Professor Krasso
 ; Date:   13 Nov 2020
 ; Modified By: Grayton Savickas
 ; Description: Utilize Arrays
+; Attribution: JavaScript: The Definitive Guide, 7th Edition by David Flanagan Published by O'Reilly Media, Inc., 2020 
 ;===========================================
 */
 
 const header = require('../week-2/Savickas-header')
-let headerValue = header.display("Grayton", "Savickas", "Header")
-console.log(headerValue);
+let headerValue = header.display("Grayton", "Savickas", "Exercise 4.2")
+console.log(headerValue + "\n");
 
+
+// Array of fruit names stored as strings
+let fruitsArray = ["Banana", "Apple", "Orange", "Tomato", "Watermelon"];
+
+/**
+* getFruit
+* Params: Array
+* Response: Console.log
+* Description: Function that iteratively console.logs evey element of the argument array
+*/
+function getFruit(Array){
+    for(let element of Array) {
+        console.log(element);
+    }    
+}
+// Expected to console.log all elements of the fruitsArray 
+getFruit(fruitsArray)
