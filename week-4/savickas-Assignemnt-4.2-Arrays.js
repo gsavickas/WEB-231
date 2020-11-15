34e57c55e610afec90bfff93c54b615efe5ca6b8
Grayton Savickas <gsavickaspro@gmail.com>
2020-11-15 12:53:29 -0600

    Added 4.2 Arrays

---
+++ week-4/savickas-Assignemnt-4.2-Arrays.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   13 Nov 2020
+; Modified By: Grayton Savickas
+; Description: Utilize Arrays
+;===========================================
+*/
+
+const header = require('../week-2/Savickas-header')
+let headerValue = header.display("Grayton", "Savickas", "Header")
+console.log(headerValue);
+
