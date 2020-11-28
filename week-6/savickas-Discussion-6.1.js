5673e55714bada931f2830d17d943ff0bde8de5b
Grayton Savickas <graytonsavickas@Mandalorian.local>
2020-11-27 19:40:18 -0600

    corrections to 6.1

---
+++ week-6/savickas-Discussion-6.1.js
@@ -0,0 +1,17 @@
+Hello
+/*
+============================================
+; Title:  Objects and build-in Objects
+; Author: Grayton Savickas
+; Date:   26 Nov 2020
+; Modified By: 
+; Description: Program that demonstrates object properties
+; Attribution: 
+; - JavaScript: The Definitive Guide, 7th Edition by David Flanagan Published by O'Reilly Media, Inc., 2020 
+;===========================================
+*/
+const header = require('../week-2/Savickas-header')
+let headerValue = header.display("Grayton", "Savickas", "Exercise 5.4")
+console.log(headerValue + "\n");
+
+// start of program
