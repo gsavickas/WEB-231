a9a6177ad5c5f1a1e726055666ec7d8ec34b1ecf
Grayton Savickas <gsavickaspro@gmail.com>
2020-11-27 23:07:39 -0600

    Changes to Discussion 6.1

--- week-6/savickas-Discussion-6.1.js
+++ week-6/savickas-Discussion-6.1.js
@@ -1,4 +1,3 @@
-Hello
 /*
 ============================================
 ; Title:  Objects and build-in Objects
@@ -11,7 +10,21 @@ Hello
 ;===========================================
 */
 const header = require('../week-2/Savickas-header')
-let headerValue = header.display("Grayton", "Savickas", "Exercise 5.4")
+let headerValue = header.display("Grayton", "Savickas", "Discussion 6.1")
 console.log(headerValue + "\n");
 
 // start of program
+
+// Creates object called tree
+let tree = new Object({type: "spruce", leaves: "green", height: 12});
+
+// displays the tree object attributes
+console.log(tree);
+
+// changes tree.height object property to 15
+tree.height = 15;
+
+// displays the tree.height object attributes
+console.log("Your tree grew to " + tree.height + " feet!");
+
+// end program
